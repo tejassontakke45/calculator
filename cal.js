9b5a7edb0b55d134f60e7478085a3240749059c0
tejassontakke45 <129364464+tejassontakke45@users.noreply.github.com>
2023-12-21 00:13:41 +0530

    Add files via upload
    that is the one of calculator build by using html, css and javascript

---
+++ cal.js
@@ -0,0 +1,4 @@
+
+    document.getElementById("myButton").onclick = function () {
+        location.href = "https://www.tutorialspoint.com/dsa_using_java/dsa_using_java_data_structures.htm";
+    };
